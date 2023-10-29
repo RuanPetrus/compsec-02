e0359e97657f2bf08bb08329a86b73d8a7e83d19
Ruan Petrus <ruan@pop-os.localdomain>
2023-10-29 16:44:18 -0300

    Add mixcolums

---
+++ mixcolums.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+MIXCOLUMNS_MATRIX = np.array([
+    [2, 3, 1, 1],
+    [1, 2, 3, 1],
+    [1, 1, 2, 3],
+    [3, 1, 1, 2],
+], np.uint8)
+
+def mix_col(col):
+    return np.dot(MIXCOLUMNS_MATRIX, col.T)
+
+def mix_cols(state):
+    return np.apply_along_axis(mix_col, axis=0, arr=state.T)
+
+def main() -> None:
+    state = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+    ], np.uint8)
+    print(mix_cols(state))
+
+if __name__ == "__main__":
+    main()
